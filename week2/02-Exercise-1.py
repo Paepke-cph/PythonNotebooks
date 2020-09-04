660daedba8fbe78ff04bfa42413ac0581e890679
Paepke <cph-bp133@cphbusiness.dk>
2020-09-04 13:30:03 +0200

    forgot something

--- week2/02-Exercise-1.py
+++ week2/02-Exercise-1.py
@@ -6,6 +6,18 @@ def print_file_content(file):
     for line in file:
         print(line.rstrip())
 
+def helper(output_file, *lines):
+    with open(output_file, "w") as file_object:
+        for line in lines:
+            if type(line) is list:
+                for element in line:
+                    if not element.endswith("\n"):
+                        file_object.write(element + "\n")
+                    else:
+                        file_object.write(element)
+            else:
+                file_object.write(line)
+
 # Writes a list of strings to the output_file.
 def write_list_to_file(output_file,lst):
     with open(output_file, "w") as file_object:
@@ -18,16 +30,15 @@ def read_file(input_file):
         contents = file_object.readlines()
         return(contents)
 
-# Sets up the argument parser
-def parser_setup():
-    parser = argparse.ArgumentParser(description="A Program that reads a file from CLI and writes it to the console and an output-file")
-    parser.add_argument('-out', '--output', default="out.txt", help="Name of the file to which the output will be written to.")
-    parser.add_argument('-in', '--input', help="Name of the file which should be opend.")
-    return(parser.parse_args())
-
 if __name__ == "__main__":
-    args = parser_setup()
-    
-    file = read_file(args.input)
-    print_file_content(file)
-    write_list_to_file(args.output, file)
+    parser = argparse.ArgumentParser(description="A Program that reads a file from CLI and writes it to the console and an output-file")
+    parser.add_argument('--output',help="Name of the file to which the output will be written to.")
+    parser.add_argument('--file', default="input.txt", help="Name of the file which should be opend.")
+    args, unknown = parser.parse_known_args()
+    file = read_file(args.file)
+    if(args.output == None):
+        print_file_content(file)
+        for element in unknown:
+            print(element + "\n")
+    else:
+        helper(args.output, file, unknown)
